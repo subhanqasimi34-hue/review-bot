2ced88510dc266832325f5b60efa6d26b4041b7e
Red_thz <subhan.qasimi34@gmail.com>
2025-11-18 18:47:26 +0100

    Delete data/data/migrations directory

--- data/data/migrations/001_init.sql
+++
@@ -1,7 +0,0 @@
--- Create initial users table
-CREATE TABLE IF NOT EXISTS users (
-    id TEXT PRIMARY KEY,
-    username TEXT,
-    discriminator TEXT,
-    created_at TEXT DEFAULT CURRENT_TIMESTAMP
-);
